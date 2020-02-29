0c98167fb0b809e75e1df35f76d22d6a97b83c87
tokizuoh <16jk112@ms.dendai.ac.jp>
2020-02-29 16:39:38 +0900

    Initial Commit

---
+++ task200202-02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  task200202-02
+//
+//  Created by 酒井 桂哉 on 2020/02/29.
+//  Copyright © 2020 tokizo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
