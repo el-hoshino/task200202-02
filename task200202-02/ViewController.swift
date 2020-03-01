a813950adc689b6f781e1bcc71d22226c4a07471
tokizuoh <16jk112@ms.dendai.ac.jp>
2020-03-01 11:17:19 +0900

    feat: 入力された文字列をひらがなに変換する機能を追加

--- task200202-02/ViewController.swift
+++ task200202-02/ViewController.swift
@@ -10,20 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var convertedTextLabel: UILabel!
+    
     var textFieldText = ""
+    
+    let api = API()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.convertedTextLabel.numberOfLines = 0
     }
     
-    @IBOutlet var textField: UITextField!
     
-    @IBAction func conbertHiragana() {
+    @IBAction func convertHiragana() {
         textFieldText = textField.text!
-        print(textFieldText)
+        print(textFieldText, textFieldText.utf8.count)
+        
+        self.api.convertHiragana(convertTextForApi: textFieldText) { (convertedStr) in
+        guard let _convertedStr = convertedStr else {
+              //コンバート失敗
+              //アラートを出す
+              return
+          }
+          DispatchQueue.main.async {
+              self.convertedTextLabel.text = _convertedStr
+          }
+        }
     }
-
-
 }
-
