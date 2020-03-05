ab9ec0d6de1f78ab6862a3ef160ae68a2d84f5cd
tokizuoh <16jk112@ms.dendai.ac.jp>
2020-03-05 17:25:22 +0900

    refactor: control+iでフォーマットした

--- task200202-02/HiraganaApi.swift
+++ task200202-02/HiraganaApi.swift
@@ -26,25 +26,25 @@ class HiraganaAPI {
         let url = "https://labs.goo.ne.jp/api/hiragana"
         let outputType = "hiragana"
         let postData = PostData(app_id: self.appID!, request_id: requestID, sentence: convertText, output_type: outputType)
-
+        
         debugPrint("(before) convert text: ", convertText)
         self.request(method: "POST", url: url, postData: postData, completion: completion)
     }
-
+    
     private func request(method: String, url: String, postData:PostData,  completion:((String?) -> Void)?) {
         guard let _url = URL(string: url) else { return }
         // URLRequstの設定
         var request = URLRequest(url: _url)
         request.httpMethod = method
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
+        
         //POSTするデータをURLRequestに持たせる
         guard let uploadData = try? JSONEncoder().encode(postData) else {
             debugPrint("json生成に失敗しました")
             return
         }
         request.httpBody = uploadData
-
+        
         //APIへPOSTしてresponseを受け取る
         let task = URLSession.shared.uploadTask(with: request, from: uploadData) { data, response, error in
             if let error = error {
@@ -65,13 +65,13 @@ class HiraganaAPI {
                 completion?(nil)
                 return
             }
-
+            
             guard response.statusCode == 200 else {
                 debugPrint("サーバエラー ステータスコード: \(response.statusCode)\n")
                 completion?(nil)
                 return
             }
-
+            
             guard let data = data, let jsonData = try? JSONDecoder().decode(Rubi.self, from: data) else {
                 debugPrint("json変換に失敗しました")
                 completion?(nil)
@@ -79,7 +79,7 @@ class HiraganaAPI {
             }
             debugPrint("(after) converted text: ", jsonData.converted)
             completion?(jsonData.converted)
-
+            
         }
         task.resume()
     }
